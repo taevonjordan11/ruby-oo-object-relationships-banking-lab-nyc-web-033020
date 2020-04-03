2d3cdb4e50ca4a3e8a784de29a4ecdda8e7dd808
Taevon Jordan <nude.plus@Taevons-MBP.home>
2020-04-02 20:51:28 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
 
+  attr_reader :name, :balance
+  attr_accessor :account, :balance, :status
+  def initialize(account)
+    @account = account
+    @name = "Avi"
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(num)
+    @balance += num
+  end
+
+  def display_balance
+    p "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :transfer_amount, :status, :amount
+
+  def initialize(sender, receiver, transfer)
+    @sender = sender
+    @receiver = receiver
+    @transfer = transfer
+    @status = "pending"
+    @amount = 50
+  end
+
+  def valid?
+     if sender.valid? && receiver.valid?
+       true
+     else
+       false
+     end
+  end
+
+  def execute_transaction
+    
+  end
+
 end
